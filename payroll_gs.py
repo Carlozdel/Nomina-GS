6e05435fa51686a66c8358ec113036eba386ec3e
Carlozdel <carlozdel@gmail.com>
2024-01-11 18:02:21 -0600

    Changed Columns as in original

--- payroll_gs.py
+++ payroll_gs.py
@@ -15,26 +15,17 @@ import itertools
 import pandas as pd
 import os
 import glob
-import pyodbc
+# import pyodbc
 import subprocess
 
 #AGREGAR LA OPCION DE CONSULTAR EL SHARED SERVER PARA PODER LEER LA JERARQUIA
 
-
-
-# import openpyxl
-# subprocess.Popen(f"ssh {'webfuser'}@{'pr-wildcat-xl09.autozone.com' } {'azwfu$r'}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-
-# f"echo {'azwfu$r'} | ssh {'webfuser'}@{'pr-wildcat-xl09.autozone.com' } "
-
-
-# df = pd.read_csv(r"./az/webfocus/ibi/apps/flatfiles/uhro/wf8/ff_uito_test3.txt")
-pd.read_csv('ftp://webfuser:azwfu$r@pr-wildcat-xl09.autozone.com/az/webfocus/ibi/apps/flatfiles/uito/wf8/ff_uito_test3.txt')
+# df_ftp = pd.read_csv('ftp://webfuser:azwfu$r@pr-wildcat-xl09.autozone.com/az/webfocus/ibi/apps/flatfiles/uito/wf8/ff_uito_test3.txt')
 
 
 # print(df)
 
-exit(0)
+
 
 # files = [f for f in os.listdir('c:/Users/cdelgad3.DZ/Downloads/') if not os.path.isdir(f)]
 # recentExc = [f for f in files if f[-5:] == '.xlsx']
@@ -52,6 +43,7 @@ latest_file = max(list_of_files, key=os.path.getctime)
 
  
 print(latest_file)
+
  
 # df = pd.read_excel('c:/Users/cdelgad3.DZ/Downloads/General Services W51 Dec 10-16, 2023.xlsx', skiprows=[0,1,2])
 df = pd.read_excel(latest_file, skiprows=[0,1,2])
@@ -82,7 +74,7 @@ frame2.pack(fill="both", expand="yes", padx=20, pady=10)
 frame3.pack(fill="both", expand="Yes", padx=20, pady=10)
 
 
-trv = ttk.Treeview(frame3, columns=(1,2,3,4,5,6,7,8,9,10,11,12), show="headings", height="18",selectmode='browse' )
+trv = ttk.Treeview(frame3, columns=(1,2,3,4,5,6,7,8,9,10,11,12,13), show="headings", height="18",selectmode='browse' )
 trv.pack()
 
 
@@ -91,27 +83,34 @@ trv.column("#2",anchor=CENTER,widt=120)
 trv.column("#3",widt=200)
 trv.column("#4",anchor=CENTER,widt=80)
 trv.column("#5",anchor=CENTER,widt=70)
-trv.column("#6",anchor=CENTER,widt=80)
-trv.column("#7",anchor=CENTER,widt=200)
-trv.column("#8",anchor=CENTER,widt=120)
-trv.column("#9",anchor=CENTER,widt=60)
-trv.column("#10",anchor=CENTER,widt=90)
-trv.column("#11",anchor=CENTER,widt=200)
-trv.column("#12",anchor=CENTER,widt=60)
 
+trv.column("#6",anchor=CENTER,widt=70)
+
+trv.column("#7",anchor=CENTER,widt=80)
+trv.column("#8",anchor=CENTER,widt=200)
+trv.column("#9",anchor=CENTER,widt=120)
+trv.column("#10",anchor=CENTER,widt=60)
+trv.column("#11",anchor=CENTER,widt=90)
+trv.column("#12",anchor=CENTER,widt=200)
+trv.column("#13",anchor=CENTER,widt=60)
+
+# exit(0)
 
 trv.heading(1, text="IGNITION")
 trv.heading(2, text="COST CENTER")
 trv.heading(3, text="FULL NAME")
 trv.heading(4, text="SHIFT")
 trv.heading(5, text="HRS WEEK")
-trv.heading(6, text="COMEDOR")
-trv.heading(7, text="MODIFIER")
-trv.heading(8, text="MODIFIED DT")
-trv.heading(9, text="FWEEK")
-trv.heading(10, text="APPROVED")
-trv.heading(11, text="SUPERVISOR")
-trv.heading(12, text="HOLIDAY")
+
+trv.heading(6, text="PDOM")
+
+trv.heading(7, text="COMEDOR")
+trv.heading(8, text="MODIFIER")
+trv.heading(9, text="MODIFIED DT")
+trv.heading(10, text="FWEEK")
+trv.heading(11, text="APPROVED")
+trv.heading(12, text="SUPERVISOR")
+trv.heading(13, text="HOLIDAY")
 
 
 lbl = Label(frame2, width=10, text="", font=("", 13) )
@@ -193,27 +192,17 @@ lbl9.grid(column=4, row=4, padx=5, pady=3)
 ent9 = ttk.Combobox(frame1, value=('YES', 'NO'),font=("", 11), justify=CENTER)
 ent9.grid(column=5, row=4, padx=5, pady=3)
 
-# modify_var  = ent1 + "," + ent2 + "," + ent3 + "," + ent4 + "," + ent5 + "," + ent6
-
-agregar_btn = Button(frame1, width=10, text="Approve All",font=("", 13) )
-agregar_btn.grid(column=6, row=4, padx=5, pady=3)
 
 agregar_btn = Button(frame1, width=10, text="Find",font=("", 13) ,
     command=lambda: query_display(ent3.get()))
-agregar_btn.grid(column=6, row=0, padx=5, pady=3)
-
-
-agregar_btn = Button(frame1, width=10, text="Add",font=("", 13) ,
-    command=lambda: query_agregar("3," + ent1.get() + "," + ent2.get() + "," + ent3.get() + "," + ent4.get() + "," + ent5.get() + "," + ent6.get()))
 agregar_btn.grid(column=6, row=1, padx=5, pady=3)
- 
+
 actualizar_btn = Button(frame1, width=10, text="Modify",font=("", 13),
     command=lambda: query_agregar("4," + ent1.get() + "," + ent2.get() + "," + ent3.get() + "," + ent4.get() + "," + ent5.get() + "," + ent6.get()))
 actualizar_btn.grid(column=6, row=2, padx=5, pady=3)
   
-eliminar_btn = Button(frame1, width=10, text="Remove",font=("", 13))
-eliminar_btn.grid(column=6, row=3, padx=5, pady=3)
-# id=1
+agregar_btn = Button(frame1, width=10, text="Approve All",font=("", 13) )
+agregar_btn.grid(column=6, row=3, padx=5, pady=3)
 
 
 def query_buscar(id):
@@ -251,41 +240,7 @@ def query_agregar(id):
     new_data = mydata_2
     splitted = new_data.split(",")
     # print(splitted)
-    if splitted[0] == '3':
-        msg_box = messagebox.askokcancel("Adding Info","You are attempting to add a new record to the DB")
-        print(msg_box)
-
-        if msg_box == True:
-            # for i in range(1, 6):
-                if splitted[1] == "" or  splitted[2] == ""  or  splitted[3] == "" or  splitted[4] == "" or  splitted[5] == "" or  splitted[6] == "":
-                    tk.messagebox.showinfo('Addition Cancelled','There are empty fields in your addition, employee has not been added to DB')
-                    # return
-                else:
-
-                    cursorDB.execute("INSERT INTO ALL_EMPLOYEES  VALUES (" + splitted[1] + " ," + splitted[2] + " ," + "'" + splitted[3] +  "'"  + " ," +  "'" + splitted[4] + "'" + " ," + splitted[5] + " ," + splitted[6] + ",'','','','','','')")
-                    connection.commit()
-                    tk.messagebox.showinfo('Addition Completed', 'You virtually added a new record')
-                    for row in trv.get_children():
-                        trv.delete(row)
-                    ent.delete(0, 'end')
-                    ent1.delete(0, 'end')
-                    ent2.delete(0, 'end')
-                    ent3.delete(0, 'end')
-                    ent4.delete(0, 'end')
-                    ent5.delete(0, 'end')
-                    ent6.delete(0, 'end')
-                    query = " SELECT * FROM ALL_EMPLOYEES WHERE IGNITION = " + splitted[1]
-                    query_res = cursorDB.execute(query )
-                    Query_result = list(query_res)
-
-                    for row in Query_result:
-                        trv.insert("",'end',text=row[0],values=list(row)) 
-                    # connection.close()
-                    return
-        else:   
-            tk.messagebox.showinfo('Return', 'You cancelled the adding process')
-            return
-    elif splitted[0] == '4':
+    if splitted[0] == '4':
 
 
         if splitted[1] == '':
@@ -310,13 +265,12 @@ def query_agregar(id):
                 var_init = split_data[0][1:]
                 var_final = split_data[9][:-1]
                 print(var_init,var_final)
-                query_to_add = "INSERT INTO ALL_EMPLOYEES  VALUES (" + var_init + " ," + "'" + splitted[2] + "'" + " ," +  "'" + splitted[3] +  "'" + " ," +  "'" + splitted[4]  +  "'"  + " ,"  + splitted[5]  + " ," + splitted[6] + " ," +  "'None'"  + " ," + "'None'"  + " ," + "'None'"  + " ," + "'None'"  + " ," + "'None'" +  "'None'" + ")"    
+                query_to_add = "INSERT INTO ALL_EMPLOYEES  VALUES (" + var_init + " ," + "'" + splitted[2] + "'" + " ," +  "'" + splitted[3] +  "'" + " ," +  "'" + splitted[4]  +  "'"  + " ,"  + splitted[5]  + " ," + splitted[6] + " ," + splitted[7] + " ," +  "'None'"  + " ," + "'None'"  + " ," + "'None'"  + " ," + "'None'"  + " ," + "'None'" +  "'None'" + ")"    
                 print("--------------------------------------")
                 print(query_to_add)
                                     
                 cursorDB.execute(query_to_add)
 
-                    
                 connection.commit()                    
 
 def query_display(id):
@@ -368,7 +322,7 @@ def query_display(id):
     ent2.insert(0,split_data_2[1])
     ent4.insert(0,split_data_2[3])
     ent5.insert(0,split_data_2[4])
-    ent6.insert(0,split_data_2[5])
+    ent6.insert(0,split_data_2[6])
     ent7.insert(0,split_data_2[11])
     ent8.insert(0,split_data_2[10])
     ent9.insert(0,split_data_2[9])
